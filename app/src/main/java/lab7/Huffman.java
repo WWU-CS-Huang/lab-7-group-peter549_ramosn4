95183af43b04c37def908dc38870131117d7fbdb
Daniel Peterson <peter549@wwu.edu>
2025-05-29 15:59:22 -0700

    Added decode method

--- app/src/main/java/lab7/Huffman.java
+++ app/src/main/java/lab7/Huffman.java
@@ -3,34 +3,83 @@
  */
 package lab7;
 
+import heap.HashTable;
 import heap.Heap;
+
 import avl.AVL;
 
 public class Huffman {
     
-    public static AVL buildTree(Heap<CharFreq, Integer> frequencies) {
+    /**
+     * Builds a tree from a list of frequencies
+     */
+    public static AVL buildTree(Heap<StrFreq, Integer> frequencies) {
         Heap<AVL, Integer> forest = new Heap<AVL, Integer>();
         while (frequencies.size() != 0) {
-            CharFreq c = frequencies.poll();
+            StrFreq c = frequencies.poll();
             AVL tree = new AVL();
-            tree.bstInsert(c.chara.toString());
+            tree.bstInsert(c.str);
             forest.add(tree, c.freq);
         }
         while (forest.size() > 1) {
             AVL lowest = forest.poll();
             AVL secondLowest = forest.poll();
             AVL newTree = new AVL();
-            newTree.root = newTree.new Node(null, null, lowest.root, secondLowest.root);
+            int combinedFreqs = Integer.valueOf(lowest.root.word) + Integer.valueOf(secondLowest.root.word);
+            newTree.root = newTree.new Node(String.valueOf(combinedFreqs), null, lowest.root, secondLowest.root);
+            forest.add(newTree, combinedFreqs);
         }
         return forest.poll();
     }
 
-    class CharFreq {
-        Character chara;
+    /**
+     * Decodes a BitString given a tree
+     */
+    public static String decode(AVL tree, String bits) {
+        StringBuilder original = new StringBuilder();
+        AVL.Node n = tree.root;
+        for (int i = 0; i < bits.length(); i++) {
+            if (bits.charAt(i) == '0') {
+                n = n.left;
+                if (n.left == null) {
+                    original.append(n.word);
+                    n = tree.root;
+                }
+            }
+            else if (bits.charAt(i) == '1') {
+                n = n.right;
+                if (n.left == null) {
+                    original.append(n.word);
+                    n = tree.root;
+                }
+            }
+        }
+        return original.toString();
+    }
+
+    public static String encode(AVL tree, String input) {
+        StringBuilder encoded = new StringBuilder();
+
+        return encoded.toString();
+    }
+
+    private static HashTable<String, String> treeToMap(AVL tree) {
+        //table is <character, how to get to it>
+
+        return null;
+    }
+
+
+
+    /**
+     * Inner container class which contains a character represented as a String and the frequency of that String
+     */
+    class StrFreq {
+        String str;
         int freq;
 
-        public CharFreq(char c, int f) {
-            chara = c;
+        public StrFreq(String s, int f) {
+            str = s;
             freq = f;
         }
     }
