f1fd1be97f6a3d245193049769bc9357670de93b
eggaroonie <naynayplaysgame04@gmail.com>
2025-05-29 14:33:45 -0700

    creates wordFrequency.java and adds avl&heap dependencies to project

---
+++ app/src/main/java/lab7/WordFrequency.java
@@ -0,0 +1,19 @@
+/*  Naythan Ramos | CSCI 241
+ *  Lab 7 - 5.29.25
+ *  
+ *  WordFrequency.java
+ *  
+ *  given an input string, calculate the frequency of each letter in the string
+ * 
+ */
+
+package main.java.lab7;
+
+import heap.Heap;
+import avl.Avl;
+
+public class WordFrequency {
+    
+
+
+}
